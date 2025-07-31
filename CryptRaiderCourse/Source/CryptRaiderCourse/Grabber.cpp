cd9c4b8f6aae6503e32c1b3bab9771363a2faa1f
Danish <danishmeanswisdom@gmail.com>
2025-07-30 20:08:39 -0400

    Added grab and release functionality

--- CryptRaiderCourse/Source/CryptRaiderCourse/Grabber.cpp
+++ CryptRaiderCourse/Source/CryptRaiderCourse/Grabber.cpp
@@ -6,10 +6,13 @@
 #include "Engine/World.h"
 #include  "DrawDebugHelpers.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/InputComponent.h"
+
 //#include "Kismet/GameplayStatics.h"
 
 // Sets default values for this component's properties
-UGrabber::UGrabber()
+UGrabber::UGrabber(): InputMappingContext(nullptr), GrabInputAction(nullptr)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -24,22 +27,15 @@ void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// Accessing Enhanced Input System through C++ and adding a mapping context through code
-	if (APlayerController* PlayerController = Cast<APlayerController>(GetWorld()->GetFirstPlayerController()))
-	{
-		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
-		if (Subsystem)
-		{
-			Subsystem->AddMappingContext(InputMappingContext,0);
+	SetupMappingContext();
 
-			UE_LOG(LogTemp, Warning, TEXT("Mapping Context Added"));
+	UInputComponent* PlayerInputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	SetupPlayerInputComponent(PlayerInputComponent);
 
-			bool HasMC = Subsystem->HasMappingContext(InputMappingContext);
-			UE_LOG(LogTemp, Warning, TEXT("Has the Mapping Context: %s"), (HasMC ? TEXT("True") : TEXT("False")));
-		}
-	}
 	
+
 }
+
 
 
 // Called every frame
@@ -47,10 +43,59 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
+
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr){return;}
+
+	if (PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		FVector TargetLocation = GetComponentLocation() + (GetForwardVector() * HoldDistance);
+		PhysicsHandle->SetTargetLocationAndRotation(TargetLocation, GetComponentRotation());
+	}
+}                                                                                   	
+																						
+void UGrabber::Grab()
+{
+	
+	GrabLogic();
+}
+
+void UGrabber::Release()
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr){return;}
+	
+	UE_LOG(LogTemp, Warning, TEXT("Releasing Grabbed Object"));
+
+	if (PhysicsHandle->GetGrabbedComponent() != nullptr)
+	{
+		PhysicsHandle->GetGrabbedComponent()->WakeAllRigidBodies();
+		PhysicsHandle->ReleaseComponent();
+	}
+	
+}
+
+void UGrabber::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	                                
+						                                                                                                            
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))           
+	{                                                                                                                           
+		EnhancedInputComponent->BindAction(GrabInputAction, ETriggerEvent::Started, this, &UGrabber::Grab);                   
+	}                                                                                                                           
+}
+
+void UGrabber::GrabLogic() const
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetPhysicsHandle();
+	if (PhysicsHandle == nullptr){return;}
+	
 	FVector StartLocation = GetComponentLocation();
 	FVector EndLocation = StartLocation + (GetForwardVector() * MaxGrabDistance);
-	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor(255, 0, 0), false, 10.0f);
+	DrawDebugLine(GetWorld(), StartLocation, EndLocation, FColor::Red, false, 10.0f);
+	DrawDebugSphere(GetWorld(), EndLocation, 10, 10, FColor::Blue, false, 10.0f);
 
+	
 	FHitResult Hit;
 	FCollisionShape Sphere = FCollisionShape::MakeSphere(GrabRadius);
 	bool HasHit = GetWorld()->SweepSingleByChannel(
@@ -63,14 +108,62 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	if (HasHit)
 	{
 		// FString HitName = Hit.GetActor()->GetActorLabel();
-		// UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s"), *HitName);
+		//UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s"), *HitName);
 
 		//AActor* HitActor = Hit.GetActor();
 		//UE_LOG(LogTemp, Warning, TEXT("Hit Actor: %s"), *HitActor->GetActorLabel());
+
+		//DrawDebugSphere(GetWorld(), Hit.Location, 10, 10, FColor::Purple, false, 10.0f);
+		//DrawDebugSphere(GetWorld(), Hit.ImpactPoint, 10, 10, FColor::Green, false, 10.0f);
+
+		UPrimitiveComponent* HitComponent = Hit.GetComponent();
+		HitComponent->WakeAllRigidBodies();
+		
+		// Using Physics Handle to grab objects in the world
+		PhysicsHandle->GrabComponentAtLocationWithRotation(
+			HitComponent,
+			NAME_None,
+			Hit.ImpactPoint,
+			GetComponentRotation()
+		);
 	}
 	else
 	{
-		//UE_LOG(LogTemp, Warning, TEXT("No hit Actor"));		
+		UE_LOG(LogTemp, Warning, TEXT("No hit Actor"));		
+	}
+}
+
+
+void UGrabber::SetupMappingContext()const
+{
+	
+	// Accessing Enhanced Input System through C++ and adding a mapping context through code
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetWorld()->GetFirstPlayerController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(InputMappingContext,0);
+
+			UE_LOG(LogTemp, Warning, TEXT("Mapping Context Added"));
+
+			const bool HasMC = Subsystem->HasMappingContext(InputMappingContext);
+			UE_LOG(LogTemp, Warning, TEXT("Has the Mapping Context: %s"), (HasMC ? TEXT("True") : TEXT("False")));
+		}
+	}
+
+}
+
+UPhysicsHandleComponent* UGrabber::GetPhysicsHandle() const
+{
+	UPhysicsHandleComponent* PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle != nullptr)
+	{
+		return PhysicsHandle;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Grabber requires a UPhysicsHandleComponent"));
+		return nullptr;
 	}
 
 }
--- CryptRaiderCourse/Source/CryptRaiderCourse/Grabber.h
+++ CryptRaiderCourse/Source/CryptRaiderCourse/Grabber.h
@@ -5,8 +5,12 @@
 #include "CoreMinimal.h"
 #include "InputMappingContext.h"
 #include "Components/SceneComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+
 #include "Grabber.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class CRYPTRAIDERCOURSE_API UGrabber : public USceneComponent
@@ -21,18 +25,38 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void GrabLogic() const;
+
+	UPhysicsHandleComponent* GetPhysicsHandle() const;
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UInputMappingContext* InputMappingContext = nullptr;
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* GrabInputAction;
+
 
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	UFUNCTION(BlueprintCallable)
+	void Release();
+
+	UFUNCTION(BlueprintCallable)
+	void Grab();
+
+	void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent);
+	void SetupMappingContext() const;
+	
 private:
 	UPROPERTY(EditAnywhere)
 	float MaxGrabDistance = 300;
 
 	UPROPERTY(EditAnywhere)
 	float GrabRadius = 100;
+
+	UPROPERTY(EditAnywhere)
+	float HoldDistance = 150;
 		
 };
